d4cbcfe0a2e0d75bbee56dcea9c91517d2353981
Daniel Holmer <daniel@holmer.io>
2020-07-19 01:27:14 +0200

    fixes + most played page

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ Run app.py
     python3 -m flask run --port=8080
 """
 import os
-from flask import Flask, session, request, redirect, render_template
+from flask import Flask, session, request, redirect, render_template, url_for
 from flask_session import Session
 import spotipy
 import uuid
@@ -21,6 +21,7 @@ GENIUS_CLIENT_ACCESS = "gwPJBes0Ai9I7S6_3MWXhfV09Bba30t_TpYmGoPi79GlFhHZ_W7HGogc
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(64)
 app.config['SESSION_TYPE'] = 'filesystem'
+CACHE_PATH = ".cache"
 
 Session(app)
 
@@ -28,8 +29,10 @@ Session(app)
 
 @app.route('/')
 def index():
-    cache_path = '.cache-'.join(str(uuid.uuid4()))
-    auth_manager = spotipy.oauth2.SpotifyOAuth(cache_path=cache_path, scope='user-read-currently-playing')
+
+    #cache_path = '.cache-'.join(str(uuid.uuid4()))
+    auth_manager = spotipy.oauth2.SpotifyOAuth(cache_path=CACHE_PATH, scope='user-read-currently-playing user-read-private user-top-read')
+
     if request.args.get("code"):
         print("request.args.get('code') = True")
         session['token_info'] = auth_manager.get_access_token(request.args["code"], check_cache=False)
@@ -39,14 +42,18 @@ def index():
 
     token_info = session.get('token_info')
     if not token_info:
+        print("NOT TOKEN INFO")
         auth_url = auth_manager.get_authorize_url()
         return render_template("logon.html", auth_url = auth_url)
 
     spotify = spotipy.Spotify(auth=token_info['access_token'])
     song_info = get_song_info(spotify)
+
     if not song_info:
         return render_template("no_song.html")
+
     song_info = get_lyrics(song_info)
+    display_name = get_user_info(spotify)
 
 
 
@@ -55,7 +62,7 @@ def index():
     #       f'<small><a href="/sign_out">[sign out]<a/></small></h2>' \
     #       f'<a href="/playlists">my playlists2</a>' \
          #  f'<small>currently playing: {results} </small>'
-    return render_template("index.html", song_info = song_info )
+    return render_template("index.html", song_info = song_info, display_name = display_name )
 
 @app.route('/playlists')
 def playlists():
@@ -71,13 +78,60 @@ def playlists():
     spotify = spotipy.Spotify(auth=token_info['access_token'])
     return spotify.current_user_playlists()
 '''
+
+@app.route('/top_songs')
+def top_songs():
+    token_info = session.get('token_info')
+    if not token_info:
+        return redirect('/')
+
+    spotify = spotipy.Spotify(auth=token_info['access_token'])
+    display_name = get_user_info(spotify)
+
+    result = spotify.current_user_top_tracks(time_range='short_term')
+
+    top_songs = []
+    for placement, res in enumerate(result["items"],1):
+        song_dict = {}
+        song_dict.update({"song_name": res["name"] ,
+                    "artist": res["album"]["artists"][0]["name"],
+                     "placement": placement,
+                     "cover_image": res["album"]["images"][2]["url"],
+                     })
+        top_songs.append(song_dict)
+        #print(f'{placement}, {res["album"]["artists"][0]["name"]} - {res["name"]}')
+        #print(res["name"])
+    #print(result["items"][0]["album"]["artists"]["name"])
+    print(top_songs)
+    #return spotify.current_user_playlists()
+    return render_template("top_songs.html", display_name=display_name, top_songs = top_songs)
+
+@app.route("/logout/")
+def logout():
+    session.pop('token_info', None)
+
+    #app.config['SECRET_KEY'] = os.urandom(64)
+    os.remove(CACHE_PATH)
+    return redirect(url_for('index'))
+
+def get_user_info(spotify):
+
+    result = spotify.current_user()
+    #print(result["display_name"])
+    display_name = result["display_name"]
+    return display_name
+
 def get_song_info(spotify):
     song_info={}
     result = spotify.current_user_playing_track()
     #print(result)
     if result:
+        artist_list= []
+        for artist in result["item"]["album"]["artists"]:
+            print(artist["name"])
+            artist_list.append(artist["name"])
         song_info.update({"song_name": result["item"]["name"] ,
-                    "artist": result["item"]["album"]["artists"][0]["name"],
+                    "artist": artist_list,
                      "album": result["item"]["album"]["name"],
                      "cover_image": result["item"]["album"]["images"][0]["url"],
                      "album_type" : result["item"]["album"]["album_type"],
